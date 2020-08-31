77dbfa72877777abd888b856fcfebd692c249257
amoh-godwin <amohgyebigodwin@gmail.com>
2020-08-31 20:49:43 +0000

    Update Ezekiel.py

--- Ezekiel.py
+++ Ezekiel.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 from urllib.request import urlopen
 
 
@@ -11,7 +12,8 @@ class Main():
 
     def __init__(self):
         self.FILTER_WORDS = {'': ""}
-        self.StoreSiteNameAndRepl = {}    // this stores sitename and its replacement that we used
+        self.ILLEGAL_CHARS_PATN = r'[;]'
+        self.StoreSiteNameAndRepl = {}    # this stores sitename and its replacement that we used
         self.startWebPage = ""
         self.enteredUrl = ""
         self.fixedUrl = ""  # from fix
@@ -56,34 +58,67 @@ class Main():
         else:
             print('_fix_url: err')
             return False
+        
+        # protocol
+        self._check_protocol(self.fixedUrl)
+
+        # check common name
+        self._store_common_name(self.passedUrl)
+
+        # Download
+        data = self._download_data(self.passedUrl)
+        # check if html, css or image mainly string or bytes
+        dt = self._check_type_of_data(data)
+        if dt['type'] == 'string':
+            self._store_str_data(data)
+            if dt['name'] is 'html':
+                self._parser(data)
+                self._gather_links(data)
+                self.replacedDownloadedStringData = self._replace_data(data=data)
+                self._save_data_offline(self.replacedDownloadedStringData)
+        else:
+            self._store_bytes_data(data)
 
     def get_address(self, webpage_addr):
-        # set the working address
+        # split to avoid the url duplication in some urls
+        raw_addr = [n for n in re.split(r'https://|http://', webpage_addr) if n != ''][0]
+        # remove unacceptable characters
+        nn = re.findall(self.ILLEGAL_CHARS_PATN, raw_addr)
+        # TODO
+        # Try url decoding
+        if nn:
+            print('Error. This is not a valid Url')
+            return False
 
-        self.enteredUrl = webpage_addr
+        # set the working address
+        self.enteredUrl = raw_addr
 
         return True
 
     def _fix_url(self):
         # fix the url before we can continue
-
-        return True
+        # do nothing for now
+        if self.enteredUrl:
+            self.fixedUrl = self.enteredUrl
+            return True
 
     def _store_common_name(self, addr):
-        # pass
-        pass
+        self.commonName = addr
+        return True
 
     def _check_protocol(self, web_addr):
-        # pass
-        pass
+        self.passedUrl = web_addr
+        return True
 
     def _download_data(self, link):
         # pass
-        pass
+        data = b''
+        return data
 
     def _check_type_of_data(self, data):
         # pass
-        pass
+        dt = {'type': "", 'name': ""}
+        return dt
 
     def _store_str_data(self, data):
         # pass
@@ -101,9 +136,8 @@ class Main():
         # pass
         pass
 
-    def _replace_data(self, key, data):
-        # pass
-        pass
+    def _replace_data(self, key=None, data=None):
+        return data
 
     def _save_data_offline(self, data):
         # pass
@@ -122,4 +156,4 @@ class Main():
         pass
 
 main = Main()
-main.prepare("")
+main.prepare("https://www.w3schools.com/html/default.asp/http://")
